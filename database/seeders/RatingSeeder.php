746273c365d97c0f1e493496261db56db915ba31
dewandra <you@example.com>
2025-08-20 12:51:36 +0700

    Optimizes seeding process for ratings table
    Improves the efficiency of the ratings table seeding by disabling
    foreign key checks and query logging. Reduces memory usage by
    batching inserts into smaller chunks and dynamically fetching
    book IDs for random selection.
    
    These changes enhance performance and scalability for large data
    seeding operations.

--- database/seeders/RatingSeeder.php
+++ database/seeders/RatingSeeder.php
@@ -6,32 +6,58 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Schema;
 
 class RatingSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    // public function run(): void
+    // {
+    //      $ratings = [];
+
+    //     for ($i = 0; $i < 500000; $i++) {
+    //         $ratings[] = [
+    //             'book_id' => rand(1, 100000),
+    //             'rating' => rand(1, 10),
+    //             'created_at' => now(),
+    //             'updated_at' => now(),
+    //         ];
+
+    //         if (count($ratings) === 1000) {
+    //             DB::table('ratings')->insert($ratings);
+    //             $ratings = [];
+    //         }
+    //     }
+
+    //     if (count($ratings)) {
+    //         DB::table('ratings')->insert($ratings);
+    //     }
+    // }
+
+    public function run()
     {
-         $ratings = [];
+        Schema::disableForeignKeyConstraints();
+        DB::disableQueryLog();
 
-        for ($i = 0; $i < 500000; $i++) {
-            $ratings[] = [
-                'book_id' => rand(1, 100000),
-                'rating' => rand(1, 10),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
+        $bookIds = DB::table('books')->pluck('id')->toArray();
 
-            if (count($ratings) === 1000) {
-                DB::table('ratings')->insert($ratings);
-                $ratings = [];
+        $batchSize = 10000;
+        for ($i = 0; $i < 500000; $i += $batchSize) {
+            $ratings = [];
+            for ($j = 0; $j < $batchSize; $j++) {
+                $ratings[] = [
+                    'book_id' => $bookIds[array_rand($bookIds)], 
+                    'rating' => rand(1, 10),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
             }
-        }
-
-        if (count($ratings)) {
             DB::table('ratings')->insert($ratings);
         }
+
+        Schema::enableForeignKeyConstraints();
+        DB::enableQueryLog();
     }
 }
